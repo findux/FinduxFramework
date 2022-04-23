aa3d59dca439f05507d4be4319f0583b232891a9
eyup <eyupfindikli@gmail.com>
2022-04-23 10:55:58 +0300

    Some init functions are added. #plain shader.

--- FinduxFramework/FinduxFramework/src/App.cpp
+++ FinduxFramework/FinduxFramework/src/App.cpp
@@ -16,6 +16,22 @@
 int App::windowWidth;
 int App::windowHeight;
 
+const GLchar* vertexShaderSource = "#version 330 core\n"
+"layout ( location = 0 ) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4( position.x , position.y , position.z , 1.0 );\n"
+"}";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4( 1.0f, 0.5f , 0.2f , 1.0f );\n"
+"}";
+
+GLuint shaderProgram;
+GLuint VBO, VAO;
+
 App::App()
 {
 	isRunning = false;
@@ -43,8 +59,11 @@ void App::initialize()
 	/* Request opengl 3.2 context.
 	* SDL doesn't have the ability to choose which profile at this time of writing,
 	* but it should default to the core profile */
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
 	/* Turn on double buffering with a 24bit Z buffer.
 	 * You may need to change this to 16 or 32 for your system */
@@ -72,10 +91,86 @@ void App::initialize()
 	/* Create our opengl context and attach it to our window */
 	mainContext = SDL_GL_CreateContext(window);
 
-	/* This makes our buffer swap syncronized with the monitor's vertical refresh */
-	SDL_GL_SetSwapInterval(1);
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
 
-	//SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (GLEW_OK != glewInit())
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return;
+	}
+
+	// Define the viewport dimensions
+	glViewport(0, 0, windowWidth, windowHeight);
+
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	GLint success;
+	GLchar infoLog[512];
+
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "Error! Vertex shader compilation failed: " << infoLog << std::endl;
+	}
+
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "Error! Fragment shader compilation failed: " << infoLog << std::endl;
+	}
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Error! Shader program linking failed: " << infoLog << std::endl;
+	}
+
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	GLfloat vertices[] =
+	{
+		-0.5f , -0.5f , 0.0f,
+		0.5f , -0.5f , 0.0f,
+		0.0f , 0.5f, 0.0f
+	};
+
+
+	
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindVertexArray(0);
 
 
 	//imgui
@@ -102,6 +197,11 @@ void App::destroy()
 	//ImGuiSDL::Deinitialize();
 	//ImGui::DestroyContext();
 	/* Delete our opengl context, destroy our window, and shutdown SDL */
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+
 	SDL_GL_DeleteContext(mainContext);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
@@ -145,30 +245,17 @@ void App::update()
 
 void App::render()
 {
-	//SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
-	//SDL_RenderClear(renderer);
-	//
-	//SDL_RenderPresent(renderer);
-	/* This makes our buffer swap syncronized with the monitor's vertical refresh */
-	SDL_GL_SetSwapInterval(1);
-
-	/* Clear our buffer with a red background */
-	glClearColor(1.0, 0.0, 0.0, 1.0);
+	// Clear the colorbuffer
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-	/* Swap our back buffer to the front */
-	SDL_GL_SwapWindow(window);
-	/* Wait 2 seconds */
-	SDL_Delay(2000);
 
-	/* Same as above, but green */
-	glClearColor(0.0, 1.0, 0.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	SDL_GL_SwapWindow(window);
-	SDL_Delay(2000);
 
-	/* Same as above, but blue */
-	glClearColor(0.0, 0.0, 1.0, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES,0,3);
+	glBindVertexArray(0);
+
+	// draw OpenGL
+
 	SDL_GL_SwapWindow(window);
-	SDL_Delay(2000);
 }
